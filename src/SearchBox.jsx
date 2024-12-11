888c31c69db692679e012f5c587da066015e7bf1
xraffay-dev <xraffay2004@gmail.com>
2024-12-11 23:57:24 +0500

    Completed City Search Component

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./SearchBox.css";
 
 export default function SearchBox() {
+  const [cityName, setCity] = useState("");
+
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(cityName);
+    setCity("");
+  };
   return (
     <div className="SearchBox">
-      <form>
-        <TextField id="City" label="City" variant="outlined" required />
-        <Button variant="contained">Search</Button>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="City"
+          label="City"
+          variant="outlined"
+          required
+          value={cityName}
+          onChange={handleInputChange}
+        />
+        <Button type="submit" variant="contained">
+          Search
+        </Button>
       </form>
     </div>
   );
